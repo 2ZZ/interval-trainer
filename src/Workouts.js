b08ef2172d6a13cae072a755d0f8d1a069280cb2
Ian Driver <driver@hotmail.co.uk>
2023-09-16 10:43:53 +0100

    Update Workouts.js

--- src/Workouts.js
+++ src/Workouts.js
@@ -86,6 +86,141 @@ export default function getWorkouts() {
       ],
     },
     {
+      name: "45-min Leg Workout 1",
+      sets: 2,
+      workTime: 40,
+      restTime: 20,
+      exercises: [
+        {
+          name: "Squat",
+          video: "male-barbell-pause-box-squat-front_GEhqEjs.mp4",
+        },
+        { name: "Alternative Lunges", video: "alternative-lunges.mp4" },
+        { name: "Goblet Squat", video: "male-dumbbell-goblet-squat-front.mp4" },
+        {
+          name: "Deadlift",
+          video: "male-barbell-snatch-grip-deadlift-front.mp4",
+        },
+        { name: "Split Squat", video: "male-dumbbell-split-squat-front.mp4" },
+        {
+          name: "Dumbbell Stiff Legged Deadlift",
+          video: "male-barbell-stiff-leg-deadlift-front.mp4",
+        },
+      ],
+    },
+    {
+      name: "45-min Leg Workout 2",
+      sets: 2,
+      workTime: 40,
+      restTime: 20,
+      exercises: [
+        { name: "Squat Press", video: "squat-press.mp4" },
+        { name: "Burpee Deadlift", video: "burpee-deadlift.mp4" },
+        {
+          name: "Squat",
+          video: "male-barbell-pause-box-squat-front_GEhqEjs.mp4",
+        },
+        { name: "Alternative Lunges", video: "alternative-lunges.mp4" },
+        { name: "Goblet Squat", video: "male-dumbbell-goblet-squat-front.mp4" },
+        { name: "Split Squat", video: "male-dumbbell-split-squat-front.mp4" },
+      ],
+    },
+    {
+      name: "45-min Arm Workout 1 (Updated)",
+      sets: 2,
+      workTime: 40,
+      restTime: 20,
+      exercises: [
+        {
+          name: "Shoulder Press",
+          video: "male-dumbbell-overhead-press-front.mp4",
+        },
+        { name: "Curls", video: "male-dumbbell-twisting-curl-front.mp4" },
+        { name: "Hammer Curl", video: "male-dumbbell-hammer-curl-front.mp4" },
+        { name: "Lunge curl RH", video: "lunge-curl-lh.mp4" },
+        { name: "Push-ups", video: "push-ups.mp4" },
+        {
+          name: "Dumbbell Skullcrusher / Tricep Extension",
+          video: "male-dumbbell-skullcrusher-front_hgKANkM.mp4",
+        },
+      ],
+    },
+    {
+      name: "45-min Arm Workout 2",
+      sets: 2,
+      workTime: 40,
+      restTime: 20,
+      exercises: [
+        {
+          name: "Shoulder Press",
+          video: "male-dumbbell-overhead-press-front.mp4",
+        },
+        { name: "Curls", video: "curls.mp4" },
+        { name: "Hammer Curl", video: "male-dumbbell-hammer-curl-front.mp4" },
+        { name: "Floor to Ceiling Press", video: "floor-to-ceiling-press.mp4" },
+        {
+          name: "Lateral Raise",
+          video: "male-dumbbell-lateral-raise-front.mp4",
+        },
+        {
+          name: "Dumbbell Rear Delt Fly",
+          video: "male-dumbbell-rear-delt-fly-front.mp4",
+        },
+      ],
+    },
+    {
+      name: "45-min Back Workout 1",
+      sets: 2,
+      workTime: 40,
+      restTime: 20,
+      exercises: [
+        { name: "Press-up Row", video: "press-up-row.mp4" },
+        {
+          name: "Left Hand Row",
+          video: "male-dumbbell-row-unilateral-side.mp4",
+        },
+        {
+          name: "Right Hand Row",
+          video: "male-dumbbell-row-unilateral-side.mp4",
+        },
+        {
+          name: "Deadlift",
+          video: "male-barbell-snatch-grip-deadlift-front.mp4",
+        },
+        {
+          name: "Dumbbell Stiff Legged Deadlift",
+          video: "male-barbell-stiff-leg-deadlift-front.mp4",
+        },
+        {
+          name: "Dumbbell Rear Delt Fly",
+          video: "male-dumbbell-rear-delt-fly-front.mp4",
+        },
+      ],
+    },
+    {
+      name: "45-min Back Workout 2 (Updated)",
+      sets: 2,
+      workTime: 40,
+      restTime: 20,
+      exercises: [
+        { name: "Press-up Row", video: "press-up-row.mp4" },
+        { name: "Burpee Deadlift", video: "burpee-deadlift.mp4" },
+        { name: "Push-ups", video: "push-ups.mp4" },
+        {
+          name: "Left Hand Row",
+          video: "male-dumbbell-row-unilateral-side.mp4",
+        },
+        {
+          name: "Right Hand Row",
+          video: "male-dumbbell-row-unilateral-side.mp4",
+        },
+        {
+          name: "Deadlift",
+          video: "male-barbell-snatch-grip-deadlift-front.mp4",
+        },
+      ],
+    },
+    {
       name: "Debug",
       sets: 2,
       workTime: 5,
