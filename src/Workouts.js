65753d3bd07ac05f99b49f2e64ca336fbbdb3c39
Ian Driver <driver@hotmail.co.uk>
2023-09-19 21:53:00 +0100

    Update Workouts.js

--- src/Workouts.js
+++ src/Workouts.js
@@ -127,21 +127,24 @@ export default function getWorkouts() {
     },
     {
       name: "Arm Workout 1",
-      sets: 4,
-      workTime: 40,
-      restTime: 20,
+      sets: 5,
+      workTime: 30,
+      restTime: 30,
       exercises: [
         {
           name: "Shoulder Press",
           video: "male-dumbbell-overhead-press-front.mp4",
         },
         { name: "Curls", video: "male-dumbbell-twisting-curl-front.mp4" },
+        {
+          name: "Diamond Pushups",
+          video: "male-bodyweight-diamond-pushup-front.mp4",
+        },
         { name: "Hammer Curl", video: "male-dumbbell-hammer-curl-front.mp4" },
-        { name: "Lunge curl RH", video: "lunge-curl-lh.mp4" },
         { name: "Push-ups", video: "push-ups.mp4" },
         {
           name: "Dumbbell Skullcrusher / Tricep Extension",
-          video: "male-dumbbell-skullcrusher-front_hgKANkM.mp4",
+          video: "male-dumbbell-skullcrusher-side_bgn7Uzz.mp4",
         },
       ],
     },
